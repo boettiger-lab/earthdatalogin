ca7f537fa0f48b029f0c86d0cfcf14d987327ef7
Carl <cboettig@gmail.com>
2023-09-11 18:39:06 +0000

    :earth_asia:

--- inst/tutorials/render.R
+++
@@ -1,4 +0,0 @@
-
-knitr::knit("inst/tutorials/gdalcubes-stac-cog.Rmd", "vignettes/gdalcubes-stac-cog.Rmd")
-knitr::knit("inst/tutorials/legacy-formats.Rmd", "vignettes/legacy-formats.Rmd")
-fs::file_move("img", "vignettes/img")
