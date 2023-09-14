1704582cfff9f1249fabf08a2759aeeb1fbafa38
Carl <cboettig@gmail.com>
2023-09-14 16:27:32 +0000

    updates

--- inst/vignette-sources/render.R
+++ inst/vignette-sources/render.R
@@ -1,4 +1,4 @@
 
 knitr::knit("inst/vignette-sources/gdalcubes-stac-cog.Rmd", "vignettes/gdalcubes-stac-cog.Rmd")
 knitr::knit("inst/vignette-sources/legacy-formats.Rmd", "vignettes/legacy-formats.Rmd")
-fs::file_move("img", "vignettes/img")
+fs::file_move("img", "vignettes/")
