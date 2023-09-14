b1b2d028d1b8ae6491a6a172c326547130c422a4
Carl <cboettig@gmail.com>
2023-09-14 21:17:00 +0000

    vignette

--- inst/vignette-sources/render.R
+++ inst/vignette-sources/render.R
@@ -1,4 +1,4 @@
 
 knitr::knit("inst/vignette-sources/gdalcubes-stac-cog.Rmd", "vignettes/gdalcubes-stac-cog.Rmd")
-#knitr::knit("inst/vignette-sources/legacy-formats.Rmd", "vignettes/legacy-formats.Rmd")
-fs::file_move("img", "vignettes/")
+knitr::knit("inst/vignette-sources/legacy-formats.Rmd", "vignettes/legacy-formats.Rmd")
+fs::file_copy(fs::dir_ls("img"), "vignettes/img", overwrite = TRUE)
