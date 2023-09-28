9d8ee449a98501e0e02d3e2723072cd38238d4d1
Carl <cobettig@gmail.com>
2023-09-28 21:50:45 +0000

    notes

--- inst/examples/us-west-2-access.R
+++ inst/examples/us-west-2-access.R
@@ -1,6 +1,10 @@
+## This example can be run only on AWS us-west2 compute, because
+## these credentials DO NOT ALLOW DATA EGRESS FROM AMAZON'S OREGON DATACENTER
+
 library(earthdatalogin)
 
 # Set auth tokens -- assumes LPDAAC, specify your DAAC!
+# NB: These tokens expire in 1-hr!
 edl_s3_token()
 
 # A random example COG from LPDAAC
