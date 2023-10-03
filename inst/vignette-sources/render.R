a2284f957435d01dd6d105b2d2c5839d7919286e
Carl <cboettig@gmail.com>
2023-10-03 17:01:38 +0000

    :cloud: :artificial_satellite:

--- inst/vignette-sources/render.R
+++ inst/vignette-sources/render.R
@@ -4,6 +4,6 @@ knitr::knit("inst/vignette-sources/gdalcubes-stac-cog.Rmd", "vignettes/gdalcubes
 knitr::knit("inst/vignette-sources/legacy-formats.Rmd", "vignettes/legacy-formats.Rmd")
 
 
-fs::file_copy(fs::dir_ls("inst/vignette-sources/img"), "vignettes/img", overwrite = TRUE)
+#fs::file_copy(fs::dir_ls("inst/vignette-sources/img"), "vignettes/img", overwrite = TRUE)
 fs::file_copy(fs::dir_ls("img"), "vignettes/img", overwrite = TRUE)
 #fs::dir_delete("img")
