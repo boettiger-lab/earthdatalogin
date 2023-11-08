a9cee4088c3ad7a32f5889dc32bb588f51992a10
Carl <cboettig@gmail.com>
2023-11-08 05:52:43 +0000

    update

--- R/search.R
+++ R/search.R
@@ -53,6 +53,14 @@ edl_search <- function(short_name = NULL,
 #'
 #' @param resp a response object from edl_search
 #' @return a character vector of URLs
+#' @export
+#' @examplesIf interactive()
+#'
+#' resp <- edl_search(short_name = "MUR-JPL-L4-GLOB-v4.1",
+#'                    temporal = c("2020-01-01", "2021-12-31"))
+#'
+#' urls <- edl_extract_urls(resp)
+#'
 edl_extract_urls <- function(resp) {
   p <- httr::content(resp, "parsed")
   all_links <- purrr::map(p$feed$entry, "links")
--- inst/examples/sst.R
+++ inst/examples/sst.R
@@ -1,7 +1,7 @@
 
-remotes::install_github("boettiger-lab/earthdatalogin@edl_search")
+# remotes::install_github("boettiger-lab/earthdatalogin@edl_search", force=TRUE)
 library(earthdatalogin)
-
+devtools::load_all()
 resp <- edl_search(short_name = "MUR-JPL-L4-GLOB-v4.1",
                    temporal = c("2020-01-01", "2020-12-31"))
 urls <- edl_extract_urls(resp)
@@ -14,7 +14,7 @@ uri <- paste0(prefix, urls, suffix)
 library(gdalcubes)
 header = edl_set_token(format="header", set_env_var = FALSE)
 gdalcubes_set_gdal_config("GDAL_HTTP_HEADERS", header)
-gdalcubes_options(parallel = 140)
+gdalcubes_options(parallel = 100)
 
 
 dates <- stringr::str_extract(basename(urls), "\\d{14}") |> lubridate::as_datetime()
