ee83b9a243eebb4873228636434f314ee1ec1e4d
Carl <cboettig@gmail.com>
2023-11-08 06:07:36 +0000

    ex

--- inst/examples/sst.R
+++ inst/examples/sst.R
@@ -1,7 +1,6 @@
 
 # remotes::install_github("boettiger-lab/earthdatalogin@edl_search", force=TRUE)
 library(earthdatalogin)
-devtools::load_all()
 resp <- edl_search(short_name = "MUR-JPL-L4-GLOB-v4.1",
                    temporal = c("2020-01-01", "2020-12-31"))
 urls <- edl_extract_urls(resp)
