c7fcc5e7716b650aaa7272bc1927ce7400a1b5d6
Carl <cboettig@gmail.com>
2023-11-09 16:58:33 +0000

    streamlined a bit

--- inst/examples/sst.R
+++ inst/examples/sst.R
@@ -2,7 +2,7 @@
 # remotes::install_github("boettiger-lab/earthdatalogin@edl_search", force=TRUE)
 library(earthdatalogin)
 resp <- edl_search(short_name = "MUR-JPL-L4-GLOB-v4.1",
-                   temporal = c("2020-01-01", "2020-12-31"))
+                   temporal = c("2020-01-01", "2021-12-31"))
 urls <- edl_extract_urls(resp)
 
 prefix <-  "vrt://NETCDF:/vsicurl/"
@@ -15,32 +15,26 @@ header = edl_set_token(format="header", set_env_var = FALSE)
 gdalcubes_set_gdal_config("GDAL_HTTP_HEADERS", header)
 gdalcubes_options(parallel = 100)
 
-
 dates <- stringr::str_extract(basename(urls), "\\d{14}") |> lubridate::as_datetime()
+cube <- stack_cube(uri, datetime_values = dates)
 
 
-bench::bench_time({
-col <- create_image_collection(uri, date_time = dates )
-})
-
-# Desired data cube shape & resolution
-v = cube_view(srs = "EPSG:4326",
-              extent = list(t0 = "2020-01-01", t1 = "2020-12-31",
-                            left = -93, right = -76,
-                            top = 41, bottom = 49),
-              nx = 1000, ny = 1000, dt = "P1M")
-
-bench::bench_time({
-raster_cube(col, v) |> gdalcubes::write_tif("~/sst2")
+bench::bench_time({ # ~ 20 min
+  cube |>
+    gdalcubes::crop(extent = list(t0 = "2020-01-01T09:00:00",
+                                  t1 = "2021-12-03T09:00:00",
+                                  left = -93, right = -76,
+                                  top = 41, bottom = 49) ) |>
+    gdalcubes::write_tif("~/sst_2yr")
 })
 
 
-f <- fs::dir_ls("~/sst2")
-dates <- stringr::str_extract(f, "\\d{4}-\\d{2}-\\d{2}")
-library(stars)
-x = read_stars(f, along=list(time=dates))
-plot(x, col = viridisLite::mako(30), nbreaks =31)
+## Now we can quickly manipulate, aggregate and plot
+files <- fs::dir_ls("~/sst_2yr")
+dates_values <- stringr::str_extract(files, "\\d{4}-\\d{2}-\\d{2}")
+cube2 <- stack_cube(files, datetime_values = dates_values)
 
+cube2 |>
+  aggregate_time("P2Y") |>
+  plot(col = viridisLite::viridis(30), nbreaks=31)
 
-# not sure why this fails
-# stack_cube(f, datetime_values = dates) |> plot(zlim=c(-27000,2100))
