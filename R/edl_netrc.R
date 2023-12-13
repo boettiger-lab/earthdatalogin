940c6aba689b4e3a969c798024af31c3c7ba8f7e
Carl <cboettig@gmail.com>
2023-12-13 23:33:46 +0000

    proper tidy of ~/.netrc
    R shouldn't write to `~/`, but old GDAL can't set a different path.  If we must write to ~/.netrc, we make sure we append to an existing ~/.netrc, make sure we append the same entry only once.  If no ~/.netrc exists, we make sure to create one as a symlink and remove it later.

--- R/edl_netrc.R
+++ R/edl_netrc.R
@@ -31,30 +31,43 @@ edl_netrc <- function(username = default("user"),
                       netrc_path = edl_netrc_path(),
                       cookie_path = edl_cookie_path()) {
 
+  # Create a .netrc for earthdatalogin
   contents <- paste("machine urs.earthdata.nasa.gov login",
                     username, "password", password)
   writeLines(contents, netrc_path)
+
+  # set GDAL env vars to use this netrc
   Sys.setenv("GDAL_HTTP_NETRC" = TRUE)
   Sys.setenv("GDAL_HTTP_NETRC_FILE" = netrc_path)  # GDAL >= 3.7.0
 
   # GDAL < 3.7 cannot use an alternative location for .netrc
-  if(!file.exists("~/.netrc")) {
+  old_gdal_compatibility(netrc_path, contents)
+
+  # Set cookie paths as GDAL env vars
+  Sys.setenv("GDAL_HTTP_COOKIEFILE" = cookie_path)
+  Sys.setenv("GDAL_HTTP_COOKIEJAR" = cookie_path)
+}
+
+
+old_gdal_compatibility <- function (netrc_path, contents) {
+  if (!file.exists("~/.netrc")) {
     file.link(netrc_path, "~/.netrc")
 
-    ## still leaves file behind?
-    e <- environment()
-    f <- function() { unlink("~/.netrc") }
+    ## clean up on exit -- packages shouldn't persist files to `~`
+    e <- .GlobalEnv
+    f <- function(e) { unlink("~/.netrc") }
     reg.finalizer(e, f, onexit = TRUE)
+
+  } else {
+    ## append to an existing netrc file -- only once
+    current <- readLines("~/.netrc")
+    if (!any(grepl("machine urs.earthdata.nasa.gov", current))) {
+      write(contents,file="~/.netrc",append=TRUE)
+    }
   }
 
-  edl_cookies(cookie_path)
-
 }
 
-edl_cookies <- function(path = edl_cookie_path()) {
-  Sys.setenv("GDAL_HTTP_COOKIEFILE" = path)
-  Sys.setenv("GDAL_HTTP_COOKIEJAR" = path)
-}
 
 edl_cookie_path <- function() {
   path <- file.path(tools::R_user_dir("earthdatalogin"), "urs_cookies")
