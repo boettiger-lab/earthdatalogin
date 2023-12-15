13bda2a8a9a0c90ab8f041c5c9857e5f0c72d933
Carl <cboettig@gmail.com>
2023-12-15 18:03:01 +0000

    :broom:

--- tests/testthat/test-edl_download.R
+++ tests/testthat/test-edl_download.R
@@ -40,7 +40,7 @@ test_that("edl_download via token", {
 
 
 
-test_that("edl_download, netrc+curl", {
+test_that("edl_download, netrc+libcurl", {
 
   skip_on_cran()
   skip_if_offline()
@@ -50,7 +50,7 @@ test_that("edl_download, netrc+curl", {
   url <- lpdacc_example_url()
   f <- tempfile(fileext = ".tif")
 
-  edl_download(url, f, auth = "netrc", method = "curl", quiet = TRUE)
+  edl_download(url, f, auth = "netrc", method = "httr", quiet = TRUE)
 
   expect_true(file.exists(f))
   r <- terra::rast(f)
