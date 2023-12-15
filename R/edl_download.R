01c1c3c0722cf231df52fb16f490578e6949812a
Carl <cboettig@gmail.com>
2023-12-15 04:22:04 +0000

    stick with base R download...

--- R/edl_download.R
+++ R/edl_download.R
@@ -11,6 +11,7 @@
 #' @param auth the authentication method ("token" for Bearer tokens
 #' or "netrc" for netrc.)
 #' @param method The download method, either "httr" or "curl".
+#' @param quiet logical default TRUE. Show progress in download?
 #' @inheritParams edl_netrc
 #' @param ... additional arguments to `download.file()`, e.g. quiet = TRUE.
 #' @return the `dest` path, invisibly
@@ -21,43 +22,53 @@
 edl_download <- function(href,
                          dest = basename(href),
                          auth = "netrc",
-                         method = "httr",
+                         method = "curl",
                          username = default("user"),
                          password = default("password"),
                          netrc_path = edl_netrc_path(),
                          cookie_path = edl_cookie_path(),
+                         quiet = TRUE,
                          ...) {
 
   if (auth == "token") {
 
-    download_using_token(href, dest, method, ...)
+    download_using_token(href, dest, method, quiet = quiet, ...)
 
   } else {
 
-    edl_netrc(username = default("user"),
-              password = default("password"),
-              netrc_path = edl_netrc_path(),
-              cookie_path = edl_cookie_path(),
+    edl_netrc(username = username,
+              password = password,
+              netrc_path = netrc_path,
+              cookie_path = cookie_path,
               cloud_config = FALSE)
 
 
     if (method == "httr") {
-      pw <- openssl::base64_encode(paste0(username, ":", password))
-      httr::GET(href,
-                httr::config(#netrc_file = netrc_path,
+
+      if(!getOption("earthdata.download.fallback", FALSE)) {
+        httr::GET(href,
+                httr::config(netrc_file = netrc_path,
                              cookiefile = cookie_path,
                              cookiejar = cookie_path),
-                httr::add_headers(Authorization= paste("Basic", pw)),
                 httr::write_disk(dest, overwrite = TRUE))
 
-    } else {
+      } else {
+        pw <- openssl::base64_encode(paste0(username, ":", password))
+        httr::GET(href,
+                  httr::config(cookiefile = cookie_path,
+                               cookiejar = cookie_path),
+                  httr::add_headers(Authorization= paste("Basic", pw)),
+                  httr::write_disk(dest, overwrite = TRUE))
+      }
 
+    } else {
       ## check syntax, curl_args not working properly
       curl_args <- paste("--netrc-file", netrc_path,
                          "-b", cookie_path,
-                         "-c", cookie_path)
+                         "-c", cookie_path,
+                         "-L")
       utils::download.file(href, dest, method = "curl",
-                           extra = curl_args, ...)
+                           extra = curl_args, quiet = quiet, ...)
     }
   }
   invisible(dest)
@@ -67,7 +78,7 @@ edl_download <- function(href,
 
 
 
-download_using_token <- function(href, dest, method, ...) {
+download_using_token <- function(href, dest, method, quiet, ...) {
   header <- edl_set_token()
   bearer <- paste("Bearer", header)
   if (method == "httr") {
@@ -75,7 +86,7 @@ download_using_token <- function(href, dest, method, ...) {
               httr::write_disk(dest, overwrite = TRUE),
               httr::add_headers(Authorization = bearer))
   } else {
-    utils::download.file(href, dest, ...,
+    utils::download.file(href, dest, quiet = quiet, ...,
                          header = list(Authorization = bearer))
   }
 }
--- tests/testthat/test-edl_download.R
+++ tests/testthat/test-edl_download.R
@@ -3,11 +3,10 @@ test_that("edl_download", {
   skip_on_cran()
   skip_if_offline()
 
+  edl_unset_netrc()
+
   url <- lpdacc_example_url()
   f <- tempfile(pattern = "HSL", fileext = ".tif")
-
-  #edl_netrc()
-  #r1 <- terra::rast(url, vsi=TRUE)
 
   edl_download(url, f)
 
@@ -24,6 +23,7 @@ test_that("edl_download via token", {
 
   skip_on_cran()
   skip_if_offline()
+  edl_unset_netrc()
 
   url <- lpdacc_example_url()
   f <- tempfile(fileext = ".tif")
@@ -44,12 +44,13 @@ test_that("edl_download, netrc+curl", {
 
   skip_on_cran()
   skip_if_offline()
-  skip("not implemented")
+
+  edl_unset_netrc()
 
   url <- lpdacc_example_url()
   f <- tempfile(fileext = ".tif")
 
-  edl_download(url, f, auth = "netrc", method = "curl")
+  edl_download(url, f, auth = "netrc", method = "curl", quiet = TRUE)
 
   expect_true(file.exists(f))
   r <- terra::rast(f)
@@ -59,52 +60,4 @@ test_that("edl_download, netrc+curl", {
 
 })
 
-
-
-
-
-
-test_that("httr download via auth", {
-
-  skip_on_cran()
-  skip_if_offline()
-
-
-  edl_netrc()
-  dest <- tempfile(fileext = ".tif")
-  url <- lpdacc_example_url()
-  r <- httr::HEAD(url,
-                  httr::config(netrc_file = edl_netrc_path(),
-                               cookiefile = edl_cookie_path(),
-                               cookiejar = edl_cookie_path()))
-
-
-  expect_true(httr::status_code(r) < 300)
-
-  username = default("user")
-  password = default("password")
-  pw <- openssl::base64_encode(paste0(username, ":", password))
-  r <- httr::GET(url,
-                 httr::config(#netrc_file = netrc_path,
-                   cookiefile = edl_cookie_path(),
-                   cookiejar = edl_cookie_path()),
-                 httr::add_headers(Authorization= paste("Basic", pw)),
-                 httr::write_disk(dest, overwrite = TRUE)
-  )
-
-  expect_true(httr::status_code(r) < 300)
-
-
-  # netrc_file does not seem to be respected
-  # attempts non-basic auth instead,
-  # though succeeds anyway if cookie is already set
-
-    r <- httr::GET(url,
-                   httr::config(netrc_file = edl_netrc_path(),
-                                cookiefile = edl_cookie_path(),
-                                cookiejar = edl_cookie_path()),
-                   httr::write_disk(dest, overwrite = TRUE))
-    expect_true(httr::status_code(r) < 300)
-
-})
 
