df11b58f3c076e656efd32e87e8bc5fb4d3139c9
Andy Teucher <andy.teucher@gmail.com>
2024-04-08 15:38:39 -0700

    recursion

--- R/edl_nasa_collections.R
+++ R/edl_nasa_collections.R
@@ -1,31 +1,42 @@
 collections_fetch <- function(collections) {
   
   next_url <- get_next_url(collections)
-  base_url <- unlist(Filter(function(x) x$rel == "self" & !is.null(x$href), collections$links))["href"]
   
-  while (!is.null(next_url)) {
-    qry <- httr::parse_url(next_url)
-
-    colls <- list(
-      version = NULL,
-      base_url = base_url,
-      endpoint = NULL,
-      params = qry$query,
-      verb = "GET",
-      encode = NULL
-    )
-    class(colls) <- c("collections", "rstac_query")
-    
-    prev_collections <- collections$collections
-    
-    collections <- rstac::get_request(colls)
-    
-    collections$collections <- c(prev_collections, collections$collections)
-    next_url <- get_next_url(collections)
-  }
-  collections
+  # If that's all there is, return it
+  if (is.null(next_url)) return(collections)
+  
+  base_url <- 
+  
+  qry <- httr::parse_url(next_url)
+  
+  colls <- list(
+    version = NULL,
+    base_url = base_url,
+    endpoint = NULL,
+    params = qry$query,
+    verb = "GET",
+    encode = NULL
+  )
+  class(colls) <- c("collections", "rstac_query")
+  
+  prev_collections <- collections$collections
+  
+  collections <- rstac::get_request(colls)
+  
+  collections$collections <- c(prev_collections, collections$collections)
+  
+  collections_fetch(collections)
 }
 
 get_next_url <- function(collections) {
   unlist(Filter(function(x) x$rel == "next", collections$links))["href"]
+}
+
+get_base_url <- function(collections) {
+  unlist(
+    Filter(
+      function(x) x$rel == "self" & !is.null(x$href), 
+      collections$links
+    )
+  )["href"]
 }
