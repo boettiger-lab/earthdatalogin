5ec2bd2b926f2a9fc6e3c15914cfc58a1b8efdff
Andy Teucher <andy.teucher@gmail.com>
2024-04-08 15:30:35 -0700

    change to collections_fetch()

--- R/edl_nasa_collections.R
+++ R/edl_nasa_collections.R
@@ -1,23 +1,29 @@
-edl_nasa_collections <- function(q) {
-  colls <- rstac::collections(q, collection_id = NULL)
-  colls_resp <- rstac::get_request(colls)
+collections_fetch <- function(collections) {
   
-  next_url <- get_next_url(colls_resp)
+  next_url <- get_next_url(collections)
+  base_url <- unlist(Filter(function(x) x$rel == "self" & !is.null(x$href), collections$links))["href"]
   
   while (!is.null(next_url)) {
     qry <- httr::parse_url(next_url)
-  
-    colls$endpoint <- basename(qry$path)
-    colls$params <- qry$query
 
-    prev_collections <- colls_resp$collections
-  
-    colls_resp <- rstac::get_request(colls)
-
-    colls_resp$collections <- c(prev_collections, colls_resp$collections)
-    next_url <- get_next_url(colls_resp)
+    colls <- list(
+      version = NULL,
+      base_url = base_url,
+      endpoint = NULL,
+      params = qry$query,
+      verb = "GET",
+      encode = NULL
+    )
+    class(colls) <- c("collections", "rstac_query")
+    
+    prev_collections <- collections$collections
+    
+    collections <- rstac::get_request(colls)
+    
+    collections$collections <- c(prev_collections, collections$collections)
+    next_url <- get_next_url(collections)
   }
-  colls_resp
+  collections
 }
 
 get_next_url <- function(collections) {
