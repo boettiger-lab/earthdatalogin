7bc913f1df0c3e4851981c6b4ac758dc3fb7dc93
Carl Boettiger <cboettig@gmail.com>
2024-05-07 22:02:02 +0000

    hdf5

--- tests/testthat/test-edl_netrc.R
+++ tests/testthat/test-edl_netrc.R
@@ -24,15 +24,18 @@ test_that("edl_netrc", {
 test_that("netcdf access", {
 
   #skip_on_os("windows") # lacks netcdf vsi support?
+  url <- "https://archive.podaac.earthdata.nasa.gov/podaac-ops-cumulus-protected/AVHRR_OI-NCEI-L4-GLOB-v2.1/20200115120000-NCEI-L4_GHRSST-SSTblend-AVHRR_OI-GLOB-v02.0-fv02.1.nc"
+  edl_netrc()
 
+  driver <- NULL
   arch <- tolower(Sys.info()[["sysname"]])
   if(arch == "windows"){
-    Sys.setenv(GDAL_SKIP="netCDF")
+    warning("Windows detected, using HDF5")
+    driver <- "HDF5"
+    # Sys.setenv(GDAL_SKIP="netCDF") # doesn't work
   }
-  url <- "https://archive.podaac.earthdata.nasa.gov/podaac-ops-cumulus-protected/AVHRR_OI-NCEI-L4-GLOB-v2.1/20200115120000-NCEI-L4_GHRSST-SSTblend-AVHRR_OI-GLOB-v02.0-fv02.1.nc"
 
-  edl_netrc()
-  r <- terra::rast(url, vsi=TRUE)
+  r <- terra::rast(url, vsi=TRUE, drivers=driver)
   expect_true(inherits(r, "SpatRaster"))
 
   edl_unset_netrc()
