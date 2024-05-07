d27377aad990391f3c10bde6255fec1dd8206674
Carl Boettiger <cboettig@gmail.com>
2024-05-07 19:38:43 +0000

    s3 test

--- tests/testthat/test-edl_s3.R
+++ tests/testthat/test-edl_s3.R
@@ -1,16 +1,18 @@
 
 test_that("edl_s3", {
 
+  skip("Only works inside AWS US-West-2")
   skip_on_cran()
   skip_if_offline()
   # tokens can only be used inside S3
-  p <-  edl_s3_token(prompt_for_netrc = FALSE)
+  p <- edl_s3_token(prompt_for_netrc = FALSE)
   expect_type(p, "list")
 })
 
 
 test_that("edl_unset_s3", {
 
+  skip("Only works inside AWS US-West-2")
   x <- edl_unset_s3()
   expect_true(x)
 
