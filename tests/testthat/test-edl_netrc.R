7ca854773e6a0b18b3c42ccdd86c1c3dd41dc7c9
Carl Boettiger <cboettig@gmail.com>
2024-05-07 19:57:01 +0000

    test netcdf too?

--- tests/testthat/test-edl_netrc.R
+++ tests/testthat/test-edl_netrc.R
@@ -19,3 +19,16 @@ test_that("edl_netrc", {
   edl_unset_netrc()
 
   })
+
+
+test_that("netcdf access", {
+
+  url <- "https://archive.podaac.earthdata.nasa.gov/podaac-ops-cumulus-protected/AVHRR_OI-NCEI-L4-GLOB-v2.1/20200115120000-NCEI-L4_GHRSST-SSTblend-AVHRR_OI-GLOB-v02.0-fv02.1.nc"
+
+  edl_netrc()
+  r <- terra::rast(url, vsi=TRUE)
+  expect_true(inherits(r, "SpatRaster"))
+
+  edl_unset_netrc()
+
+})
