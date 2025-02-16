fbc3a1e7ff2c6e5d4be34400e348648d59bf037b
Carl Boettiger <cboettig@gmail.com>
2025-02-16 04:14:31 +0000

    test should be robust to collection growth

--- tests/testthat/test-stac-collections_fetch.R
+++ tests/testthat/test-stac-collections_fetch.R
@@ -3,7 +3,7 @@ test_that("collections_fetch works", {
   skip_if_offline()
   skip_if_not_installed("rstac")
 
-  # The unpaged reponse > 10 records
+  # The unpaged response now has  20 records
   coll1 <-  rstac::stac("https://cmr.earthdata.nasa.gov/stac/LARC_CLOUD") |>
     rstac::collections() |>
     rstac::get_request()
@@ -12,7 +12,7 @@ test_that("collections_fetch works", {
   coll2 <- collections_fetch(coll1)
   expect_s3_class(coll2, c("doc_collections", "rstac_doc", "list"))
 
-  # At time of writing (2024-04-08) there were 16 collections in the LARC STAC
+  # At time of writing (2025-02-15) there were 70 collections in the LARC STAC
   # catalogue
   expect_gt(length(coll2$collections), 2)
 
