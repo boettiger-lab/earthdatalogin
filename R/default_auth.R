c8a72888cea6d6c2cf6861fe4108ce2b875ebfab
Carl Boettiger <cboettig@gmail.com>
2025-07-10 16:28:10 +0000

    bugfix :bug: patch default auth

--- R/default_auth.R
+++ R/default_auth.R
@@ -2,8 +2,11 @@ default <- function(what) {
 
   href <- paste0("https://raw.githubusercontent.com/",
          "boettiger-lab/earthdatalogin/main/inst/.Renviron")
-  tryCatch(readRenviron(href),
+  renviron <- tempfile("Renviron")
+  download.file(href, renviron)
+  tryCatch(readRenviron(renviron),
            warning = function(e) NULL)
+  unlink(renviron) # clean up
   switch(what,
          user = Sys.getenv("EARTHDATA_USER", "earthaccess"),
          password = Sys.getenv("EARTHDATA_PASSWORD", "EDL_test1"))
